4464a8ea9933d55d5f8d1b604aa3bea1735eaaab
Antriksh Singh <a0s00vf@homeoffice.wal-mart.com>
2021-03-23 23:25:11 +0530

    Get servable cities

--- actions/actions.py
+++ actions/actions.py
@@ -9,41 +9,49 @@ ZomatoData = ZomatoData.drop_duplicates().reset_index(drop=True)
 WeOperate = ['New Delhi', 'Gurgaon', 'Noida', 'Faridabad', 'Allahabad', 'Bhubaneshwar', 'Mangalore', 'Mumbai', 'Ranchi', 'Patna', 'Mysore', 'Aurangabad', 'Amritsar', 'Puducherry', 'Varanasi', 'Nagpur', 'Vadodara', 'Dehradun', 'Vizag', 'Agra',
              'Ludhiana', 'Kanpur', 'Lucknow', 'Surat', 'Kochi', 'Indore', 'Ahmedabad', 'Coimbatore', 'Chennai', 'Guwahati', 'Jaipur', 'Hyderabad', 'Bangalore', 'Nashik', 'Pune', 'Kolkata', 'Bhopal', 'Goa', 'Chandigarh', 'Ghaziabad', 'Ooty', 'Gangtok', 'Shimla']
 
-print(ZomatoData)
+Opearations = ZomatoData['City'].value_counts().index
+# 	['New Delhi', 'Gurgaon', 'Noida', 'Faridabad', 'Ranchi', 'Mangalore',
+#    'Aurangabad', 'Bhubaneshwar', 'Mumbai', 'Mysore', 'Amritsar',
+#    'Allahabad', 'Patna', 'Dehradun', 'Varanasi', 'Kanpur', 'Vizag',
+#    'Vadodara', 'Nagpur', 'Ludhiana', 'Puducherry', 'Agra', 'Kochi',
+#    'Lucknow', 'Indore', 'Surat', 'Coimbatore', 'Chennai', 'Bangalore',
+#    'Ahmedabad', 'Hyderabad', 'Jaipur', 'Guwahati', 'Pune', 'Kolkata',
+#    'Nashik', 'Bhopal', 'Goa', 'Chandigarh', 'Ghaziabad', 'Gangtok', 'Ooty',
+#    'Shimla', 'Mohali', 'Secunderabad', 'Nasik', 'Panchkula']
 
-# def RestaurantSearch(City, Cuisine):
-#     TEMP = ZomatoData[(ZomatoData['Cuisines'].apply(lambda x: Cuisine.lower() in x.lower())) & (
-#         ZomatoData['City'].apply(lambda x: City.lower() in x.lower()))]
-#     return TEMP[['Restaurant Name', 'Address', 'Average Cost for two', 'Aggregate rating']]
-
-
-# class ActionSearchRestaurants(Action):
-#     def name(self):
-#         return 'action_search_restaurants'
-
-#     def run(self, dispatcher, tracker, domain):
-#         #config={ "user_key":"f4924dc9ad672ee8c4f8c84743301af5"}
-#         loc = tracker.get_slot('location')
-#         cuisine = tracker.get_slot('cuisine')
-#         results = RestaurantSearch(City=loc, Cuisine=cuisine)
-#         response = ""
-#         if results.shape[0] == 0:
-#             response = "no results"
-#         else:
-#             for restaurant in RestaurantSearch(loc, cuisine).iloc[:5].iterrows():
-#                 restaurant = restaurant[1]
-#                 response = response + \
-#                     F"Found {restaurant['Restaurant Name']} in {restaurant['Address']} rated {restaurant['Address']} with avg cost {restaurant['Average Cost for two']} \n\n"
-
-#         dispatcher.utter_message("-----"+response)
-#         return [SlotSet('location', loc)]
+def RestaurantSearch(City, Cuisine):
+    TEMP = ZomatoData[(ZomatoData['Cuisines'].apply(lambda x: Cuisine.lower() in x.lower())) & (
+        ZomatoData['City'].apply(lambda x: City.lower() in x.lower()))]
+    return TEMP[['Restaurant Name', 'Address', 'Average Cost for two', 'Aggregate rating']]
 
 
-# class ActionSendMail(Action):
-#     def name(self):
-#         return 'action_send_mail'
+class ActionSearchRestaurants(Action):
+    def name(self):
+        return 'action_search_restaurants'
 
-#     def run(self, dispatcher, tracker, domain):
-#         MailID = tracker.get_slot('mail_id')
-#         sendmail(MailID, response)
-#         return [SlotSet('mail_id', MailID)]
+    def run(self, dispatcher, tracker, domain):
+        #config={ "user_key":"f4924dc9ad672ee8c4f8c84743301af5"}
+        loc = tracker.get_slot('location')
+        cuisine = tracker.get_slot('cuisine')
+        results = RestaurantSearch(City=loc, Cuisine=cuisine)
+        response = ""
+        if results.shape[0] == 0:
+            response = "no results"
+        else:
+            for restaurant in RestaurantSearch(loc, cuisine).iloc[:5].iterrows():
+                restaurant = restaurant[1]
+                response = response + \
+                    F"Found {restaurant['Restaurant Name']} in {restaurant['Address']} rated {restaurant['Address']} with avg cost {restaurant['Average Cost for two']} \n\n"
+
+        dispatcher.utter_message("-----"+response)
+        return [SlotSet('location', loc)]
+
+
+class ActionSendMail(Action):
+    def name(self):
+        return 'action_send_mail'
+
+    def run(self, dispatcher, tracker, domain):
+        MailID = tracker.get_slot('mail_id')
+        sendmail(MailID, response)
+        return [SlotSet('mail_id', MailID)]
