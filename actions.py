b580b53d0dc8d9d90c071293870026b6f3b5499d
Shouvik Roy <shouvikroy@outlook.com>
2021-03-23 21:58:09 +0530

    Retrieve slot price

--- actions.py
+++ actions.py
@@ -11,7 +11,8 @@ ZomatoData = pd.read_csv('zomato.csv')
 ZomatoData = ZomatoData.drop_duplicates().reset_index(drop=True)
 WeOperate = ['New Delhi', 'Gurgaon', 'Noida', 'Faridabad', 'Allahabad', 'Bhubaneshwar', 'Mangalore', 'Mumbai', 'Ranchi', 'Patna', 'Mysore', 'Aurangabad', 'Amritsar', 'Puducherry', 'Varanasi', 'Nagpur', 'Vadodara', 'Dehradun', 'Vizag', 'Agra', 'Ludhiana', 'Kanpur', 'Lucknow', 'Surat', 'Kochi', 'Indore', 'Ahmedabad', 'Coimbatore', 'Chennai', 'Guwahati', 'Jaipur', 'Hyderabad', 'Bangalore', 'Nashik', 'Pune', 'Kolkata', 'Bhopal', 'Goa', 'Chandigarh', 'Ghaziabad', 'Ooty', 'Gangtok', 'Shimla']
 
-def RestaurantSearch(City,Cuisine):
+def RestaurantSearch(City,Cuisine,Budget):
+
 	TEMP = ZomatoData[(ZomatoData['Cuisines'].apply(lambda x: Cuisine.lower() in x.lower())) & (ZomatoData['City'].apply(lambda x: City.lower() in x.lower()))]
 	return TEMP[['Restaurant Name','Address','Average Cost for two','Aggregate rating']]
 
@@ -23,12 +24,15 @@ class ActionSearchRestaurants(Action):
 		#config={ "user_key":"f4924dc9ad672ee8c4f8c84743301af5"}
 		loc = tracker.get_slot('location')
 		cuisine = tracker.get_slot('cuisine')
-		results = RestaurantSearch(City=loc,Cuisine=cuisine)
+		budget = tracker.get_slot('price')
+		
+		dispatcher.utter_message(budget)
+		results = RestaurantSearch(City=loc,Cuisine=cuisine,Budget=budget)
 		response=""
 		if results.shape[0] == 0:
 			response= "no results"
 		else:
-			for restaurant in RestaurantSearch(loc,cuisine).iloc[:5].iterrows():
+			for restaurant in RestaurantSearch(loc,cuisine,budget).iloc[:5].iterrows():
 				restaurant = restaurant[1]
 				response=response + F"Found {restaurant['Restaurant Name']} in {restaurant['Address']} rated {restaurant['Address']} with avg cost {restaurant['Average Cost for two']} \n\n"
 				
