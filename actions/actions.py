9a14f64f002f2a543eb9b490f9afbd297680cb9a
Antriksh Singh <a0s00vf@homeoffice.wal-mart.com>
2021-03-24 02:11:19 +0530

    update actions and clean up

--- actions/actions.py
+++ actions/actions.py
@@ -1,16 +1,27 @@
+from typing import Any, Dict, List
 
-from rasa_sdk import Action
+from rasa_sdk import Action, Tracker
 from rasa_sdk.events import SlotSet
+from rasa_sdk.executor import CollectingDispatcher
 import pandas as pd
 import json
 
+# Reading zomato data into a dataframe
 ZomatoData = pd.read_csv('zomato.csv')
 ZomatoData = ZomatoData.drop_duplicates().reset_index(drop=True)
-WeOperate = ['New Delhi', 'Gurgaon', 'Noida', 'Faridabad', 'Allahabad', 'Bhubaneshwar', 'Mangalore', 'Mumbai', 'Ranchi', 'Patna', 'Mysore', 'Aurangabad', 'Amritsar', 'Puducherry', 'Varanasi', 'Nagpur', 'Vadodara', 'Dehradun', 'Vizag', 'Agra',
-             'Ludhiana', 'Kanpur', 'Lucknow', 'Surat', 'Kochi', 'Indore', 'Ahmedabad', 'Coimbatore', 'Chennai', 'Guwahati', 'Jaipur', 'Hyderabad', 'Bangalore', 'Nashik', 'Pune', 'Kolkata', 'Bhopal', 'Goa', 'Chandigarh', 'Ghaziabad', 'Ooty', 'Gangtok', 'Shimla']
 
-Opearations = ZomatoData['City'].value_counts().index
-# 	['New Delhi', 'Gurgaon', 'Noida', 'Faridabad', 'Ranchi', 'Mangalore',
+# Cities from the template have some missing values like Nasik
+WeOperate = ['New Delhi', 'Gurgaon', 'Noida', 'Faridabad', 'Allahabad', 'Bhubaneshwar', 'Mangalore', 'Mumbai',
+             'Ranchi', 'Patna', 'Mysore', 'Aurangabad', 'Amritsar', 'Puducherry', 'Varanasi', 'Nagpur', 'Vadodara',
+             'Dehradun', 'Vizag', 'Agra', 'Ludhiana', 'Kanpur', 'Lucknow', 'Surat', 'Kochi', 'Indore', 'Ahmedabad',
+             'Coimbatore', 'Chennai', 'Guwahati', 'Jaipur', 'Hyderabad', 'Bangalore', 'Nashik', 'Pune', 'Kolkata',
+             'Bhopal', 'Goa', 'Chandigarh', 'Ghaziabad', 'Ooty', 'Gangtok', 'Shimla']
+
+# Getting Valid cities from the data
+WeOperate = ZomatoData['City'].value_counts().index
+
+# Here is the output of above operation
+#   ['New Delhi', 'Gurgaon', 'Noida', 'Faridabad', 'Ranchi', 'Mangalore',
 #    'Aurangabad', 'Bhubaneshwar', 'Mumbai', 'Mysore', 'Amritsar',
 #    'Allahabad', 'Patna', 'Dehradun', 'Varanasi', 'Kanpur', 'Vizag',
 #    'Vadodara', 'Nagpur', 'Ludhiana', 'Puducherry', 'Agra', 'Kochi',
@@ -19,32 +30,52 @@ Opearations = ZomatoData['City'].value_counts().index
 #    'Nashik', 'Bhopal', 'Goa', 'Chandigarh', 'Ghaziabad', 'Gangtok', 'Ooty',
 #    'Shimla', 'Mohali', 'Secunderabad', 'Nasik', 'Panchkula']
 
-def RestaurantSearch(City, Cuisine):
-    TEMP = ZomatoData[(ZomatoData['Cuisines'].apply(lambda x: Cuisine.lower() in x.lower())) & (
-        ZomatoData['City'].apply(lambda x: City.lower() in x.lower()))]
-    return TEMP[['Restaurant Name', 'Address', 'Average Cost for two', 'Aggregate rating']]
+PRICE_RANGE_MAP = {
+    'low': range(300),
+    'mid': range(300, 701),
+    'high': range(701, 10000)  # Max value in data is 8K in data
+}
+
+
+def restaurant_search(city: str, cuisine: str, budget: str, limit: int) -> pd.DataFrame:
+    """Function to Search the resultants based on the params from bots"""
+
+    return ZomatoData[(ZomatoData['Cuisines'].apply(lambda x: cuisine.lower() in x.lower())) &
+                      # Below == is used to avoid cases like Mumbai & Navi Mumbai
+                      (ZomatoData['City'].apply(lambda x: city.lower() == x.lower())) &
+                      (ZomatoData['Average Cost for two'].apply(lambda x: x in PRICE_RANGE_MAP[budget]))][
+                          ['Restaurant Name', 'Address', 'Average Cost for two',
+                           'Aggregate rating']].sort_values(by='Aggregate rating', ascending=False)[:limit]
 
 
 class ActionSearchRestaurants(Action):
-    def name(self):
+    def name(self) -> str:
+        """Defines the action's name. Used in bot domain"""
         return 'action_search_restaurants'
 
-    def run(self, dispatcher, tracker, domain):
-        #config={ "user_key":"f4924dc9ad672ee8c4f8c84743301af5"}
-        loc = tracker.get_slot('location')
+    def run(self,
+            dispatcher: CollectingDispatcher,
+            tracker: Tracker,
+            domain: Dict[str, Any]
+            ) -> List[Dict[str, Any]]:
+        """Executing the action."""
+        city = tracker.get_slot('location')
+        if city.lower() not in WeOperate:
+            """Utter no service incase of location not identified"""
+            dispatcher.utter_message(response="utter_no_service")
+            return []
         cuisine = tracker.get_slot('cuisine')
-        results = RestaurantSearch(City=loc, Cuisine=cuisine)
-        response = ""
-        if results.shape[0] == 0:
-            response = "no results"
-        else:
-            for restaurant in RestaurantSearch(loc, cuisine).iloc[:5].iterrows():
-                restaurant = restaurant[1]
-                response = response + \
-                    F"Found {restaurant['Restaurant Name']} in {restaurant['Address']} rated {restaurant['Address']} with avg cost {restaurant['Average Cost for two']} \n\n"
+        budget = tracker.get_slot('budget')
+        results = restaurant_search(city, cuisine, budget, 5)
 
-        dispatcher.utter_message("-----"+response)
-        return [SlotSet('location', loc)]
+        if results.empty:
+            """Utter no results if query returns empty"""
+            dispatcher.utter_message(response="utter_no_result")
+            return []
+
+        dispatcher.utter_message('\n'.join(results.apply(
+            lambda x: f"{x[0]} in {x[1]} has been rated {x[3]}", axis=1).values.tolist()))
+        return []
 
 
 class ActionSendMail(Action):
@@ -55,3 +86,9 @@ class ActionSendMail(Action):
         MailID = tracker.get_slot('mail_id')
         sendmail(MailID, response)
         return [SlotSet('mail_id', MailID)]
+
+
+if __name__ == "__main__":
+    # Testing few cases
+    print(restaurant_search("Nasik", "Chinese", "mid", 5))
+    print(restaurant_search("New Delhi", "Chinese", "high", 5))
